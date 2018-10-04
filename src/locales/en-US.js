3f0f95274e3f6aba4fc7b85c4a90e373d39b4fe3
gaoj1981 <www.woniu8.com@163.com>
2018-10-04 12:27:23 +0800

    国际化1

--- src/locales/en-US.js
+++ src/locales/en-US.js
@@ -98,7 +98,7 @@ export default {
   'menu.result.success': 'Success',
   'menu.result.fail': 'Fail',
   'menu.exception': 'Exception',
-  'menu.exception.not-permission': '403',
+  'menu.exception.not-permission': 'HTTP403',
   'menu.exception.not-find': '404',
   'menu.exception.server-error': '500',
   'menu.exception.trigger': 'Trigger',
