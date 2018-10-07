c6047b79d7a488d4587fded1e62129341b358a95
gaoj1981 <www.woniu8.com@163.com>
2018-10-07 17:15:54 +0800

    git

--- src/pages/Sales/Record.js
+++ src/pages/Sales/Record.js
@@ -1,18 +1,18 @@
 import React, { PureComponent } from 'react';
 import { FormattedMessage } from 'umi/locale';
 
-import styles from './Obd.less';
+import styles from './Record.less';
 
-class BaseInfo extends PureComponent {
+class Record extends PureComponent {
   componentDidMount() {}
 
   render() {
     return (
       <div className={styles.testCss}>
-        <FormattedMessage id="menu.car.obd" />
+        <FormattedMessage id="menu.sales.record" />
       </div>
     );
   }
 }
 
-export default BaseInfo;
+export default Record;
